53546e12552ddc053f29b7c5a1d1554634427997
Steph Poleij <99033734@mydavinci.nl>
2021-11-10 10:02:30 +0100

    add

---
+++ lightswitch.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+window = tk.Tk()
+
+button = tk.Button(text='...', bg="white", fg="black")
+button.pack(pady = 20, padx = 20)
+
+# schijf hier tussen je code
+
+
+
+# schijf hier tussen je code
+
+window.mainloop()
